9951bc6d66135a2d04de289fb4e05e24f07f8e58
Denis Frolov <d.fr8938@gmail.com>
2020-06-30 19:26:31 +0300

    add output

---
+++ Output/Output.java
@@ -0,0 +1,41 @@
+public class Output {
+
+	public static void main(String[] args) {
+
+		/*
+			System.out - вывод сообщений в консоль
+			System.err - вывод ошибок в консоль
+
+			Оба объекта возвращают экземпляр класса PrintStream
+			у которого доступны методы:
+			
+			print, println, printf 
+		*/
+
+		/* print
+			- отправляет данные в стандартный поток вывода
+		    - принимает все типы данных
+		    - не делает перенос строки, после вывода всего содержимого
+		*/
+
+		System.out.print("Output\n");
+		System.err.print("Error\n");
+
+		/* println
+			- отправляет данные в стандартный поток вывода
+		    - принимает все типы данных
+		    - делает перенос строки, после вывода всего содержимого
+		*/
+
+		System.out.println("Output");
+		System.err.println("Error");
+
+		/* printf
+			- предназначен для форматированного вывода данных
+		*/
+
+		System.out.println("%d", 10);
+		System.err.println("%f", 10.2569896);
+		System.err.println("%s", "Hello, world!");
+	}
+}
