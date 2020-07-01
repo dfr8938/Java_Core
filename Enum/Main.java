8c0991784cb0a1029db3f9bdc7473c9d0d450c67
Denis Frolov <d.fr8938@gmail.com>
2020-07-01 12:24:25 +0300

    add enum

---
+++ Enum/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Color colorRed, colorBlack;
+
+		colorRed = Color.RED;
+		colorBlack = Color.BLACK;
+
+		if(colorBlack == Color.RED) {
+
+			System.out.println("RED");
+		} else if(colorBlack == Color.BLACK) {
+
+			System.out.println("Black");
+		} else {
+
+			System.out.println("NoNAME");
+		}
+	}
+}
