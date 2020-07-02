9b40be66c1c320b7c38417f50247474ac6f8a025
Denis Frolov <d.fr8938@gmail.com>
2020-07-03 01:36:18 +0300

    in SingleArrays class add method reverse and outputArrays

--- 12. Arrays/SingleArrays.java
+++ 12. Arrays/SingleArrays.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class SingleArrays {
 
 	public static void main(String[] args) {
+
+		// int[] intArray = {
+		// 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+		// };
 		
 		Scanner in  = new Scanner(System.in);
 
@@ -10,23 +14,53 @@ public class SingleArrays {
 
 		int countIteration = in.nextInt();
 
-		// int[] intArray = {
-		// 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-		// };
-
-		System.out.print("\narray = [");
-
 		int[] intArray = new int[countIteration];
 
-		for (int i = 0; i < intArray.length; i++) {
-			intArray[i] = i + 1;
-			String str = i == intArray.length - 1 ? intArray[i] + "]" : intArray[i] + ", ";
-			System.out.print(str);
-		}
+		outputArray(intArray);
 
 		System.out.println("\n\nmin value = " + min(intArray));
 		System.out.println("max value = " + max(intArray));
+
+		reverse(intArray);
+
+		outputArray(intArray);
 		
+	}
+
+	public static void outputArray(int[] arr) {
+
+		System.out.print("\narray = [");
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+			String str = i == arr.length - 1 ? arr[i] + "]" : arr[i] + ", ";
+			System.out.print(str);
+		}
+	}
+
+	public static void reverse(int[] arr) {
+
+		System.out.print("\narray = [");
+
+		for(int i = arr.length - 1; i >= 0; i--) {
+
+			arr[i] = arr[i];
+			String str = i == 0 ? arr[i] + "]\n" : arr[i] + ", ";
+			System.out.print(str);
+		}
+	}
+
+	public static int min(int[] arr) {
+
+		int x = arr[0];
+
+		for(int i = 0; i < arr.length; i++) {
+			if(x > arr[i]) {
+				x = arr[i];
+			}
+		} 
+
+		return x;
 	}
 
 	public static int max(int[] arr) {
@@ -42,16 +76,4 @@ public class SingleArrays {
 		return x;
 	}
 
-	public static int min(int[] arr) {
-
-		int x = arr[0];
-
-		for(int i = 0; i < arr.length; i++) {
-			if(x > arr[i]) {
-				x = arr[i];
-			}
-		} 
-
-		return x;
-	}
 }
