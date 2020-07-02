890c8d1e20986ceb9f1e9922968fee59a859604f
Denis Frolov <d.fr8938@gmail.com>
2020-07-02 14:24:51 +0300

    add arrays

---
+++ 12. Arrays/Arrays.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		Scanner in  = new Scanner(System.in);
+
+		System.out.print("Enter the number of iterations: ");
+
+		int countIteration = in.nextInt();
+
+		// int[] intArray = {
+		// 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+		// };
+
+		int[] intArray = new int[countIteration];
+
+		for (int i = 0; i < intArray.length; i++) {
+			intArray[i] = i + 1;
+			String str = i == intArray.length - 1 ? intArray[i] + "." : intArray[i] + ", ";
+			System.out.print(str);
+		}
+	}
+}
