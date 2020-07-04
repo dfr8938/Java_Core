8ed9706f9615cba975f2b5d76ab7d9d5141e578a
Denis Frolov <d.fr8938@gmail.com>
2020-07-04 23:13:06 +0300

    modify file structure

---
+++ 00. Books/The basics Java(Prohorenok)/17. Generics/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ArrayList <String> list = new ArrayList();
+		list.add("hello");
+		String s = list.get(0);
+		System.out.println(s);
+	}
+}
