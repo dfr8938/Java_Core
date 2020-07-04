cd32db67d680b341359df6ada20b38e500d6b4b4
Denis Frolov <d.fr8938@gmail.com>
2020-07-05 01:16:47 +0300

    create two class instance

--- 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/CarManager.java
+++ 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/CarManager.java
@@ -4,5 +4,8 @@ public class CarManager {
 	
 	public static void main(String[] args) {
 		
+		Car car1 = new Car(2008, 15000, "green", 1200, "Toyota Prius");
+		Car car2 = new Car(2000, 9700, "white", 1600, "Renault Laguna"); 
+		
 	}
 }
