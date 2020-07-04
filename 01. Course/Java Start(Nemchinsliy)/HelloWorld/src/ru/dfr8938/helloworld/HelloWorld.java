2ef18083b5a3d28a9fcf89d10ee791fbdf2e2a23
Denis Frolov <d.fr8938@gmail.com>
2020-07-04 23:51:58 +0300

    add new variable message in class HelloWorld

--- 01. Course/Java Start(Nemchinsliy)/HelloWorld/src/ru/dfr8938/helloworld/HelloWorld.java
+++ 01. Course/Java Start(Nemchinsliy)/HelloWorld/src/ru/dfr8938/helloworld/HelloWorld.java
@@ -3,6 +3,9 @@ package ru.dfr8938.helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		
+		String message = "Hello world!";
+		
+		System.out.println(message);
 	}
 }
