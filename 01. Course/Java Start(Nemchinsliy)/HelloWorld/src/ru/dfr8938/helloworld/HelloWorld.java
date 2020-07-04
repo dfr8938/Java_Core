21e2b325626262691a745a66f6b44fbc332faeda
Denis Frolov <d.fr8938@gmail.com>
2020-07-05 00:06:33 +0300

    add variables answer

--- 01. Course/Java Start(Nemchinsliy)/HelloWorld/src/ru/dfr8938/helloworld/HelloWorld.java
+++ 01. Course/Java Start(Nemchinsliy)/HelloWorld/src/ru/dfr8938/helloworld/HelloWorld.java
@@ -9,5 +9,18 @@ public class HelloWorld {
 		
 		int i = 42;
 		System.out.println("int i = " + i);
+		
+		int j = 7;
+		i = 6;
+		
+		int answer1 = i + j;
+		int answer2 = i - j;
+		int answer3 = i * j;
+		int answer4 = i / j;
+		
+		System.out.println("i + j = " + answer1);
+		System.out.println("i - j = " + answer2);
+		System.out.println("i * j = " + answer3);
+		System.out.println("i / j = " + answer4);
 	}
 }
