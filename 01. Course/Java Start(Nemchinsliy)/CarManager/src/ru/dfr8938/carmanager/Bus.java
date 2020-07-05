a5b3ac779c4d7eec119c876d1f812b662d652f41
Denis Frolov <d.fr8938@gmail.com>
2020-07-05 21:08:31 +0300

    added two child classes of class Car

---
+++ 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/Bus.java
@@ -0,0 +1,10 @@
+package ru.dfr8938.carmanager;
+
+public class Bus extends Car {
+
+	public Bus(int yearOfProduction, int price, String color, int weight, String name) {
+		super(yearOfProduction, price, color, weight, name);
+		
+	}
+	
+}
---
+++ 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/PassengerCar.java
@@ -0,0 +1,10 @@
+package ru.dfr8938.carmanager;
+
+public class PassengerCar extends Car {
+
+	public PassengerCar(int yearOfProduction, int price, String color, int weight, String name) {
+		super(yearOfProduction, price, color, weight, name);
+		
+	}
+
+}
