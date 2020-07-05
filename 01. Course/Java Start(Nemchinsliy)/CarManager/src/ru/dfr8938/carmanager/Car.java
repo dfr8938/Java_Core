154a87ddbf0047ab7d9e121d3dc8124204e45b3e
Denis Frolov <d.fr8938@gmail.com>
2020-07-05 12:12:54 +0300

    add getters, setters and methods toString

--- 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/Car.java
+++ 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/Car.java
@@ -15,4 +15,55 @@ public class Car {
 		this.weight = weight;
 		this.name = name;
 	}
+	
+	// getters
+	public int getYearOfProduction() {
+		return this.yearOfProduction;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public int getWeight() {
+		return this.weight;
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	//setters
+	public void setYearOfProduction(int yearOfProduction) {
+		this.yearOfProduction = yearOfProduction;
+	}
+	
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [yearOfProduction=" + yearOfProduction + ", price=" + price + ", weight=" + weight + ", color="
+				+ color + ", name=" + name + "]";
+	}
+	
+	
+	
 }
--- 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/CarManager.java
+++ 01. Course/Java Start(Nemchinsliy)/CarManager/src/ru/dfr8938/carmanager/CarManager.java
@@ -4,8 +4,11 @@ public class CarManager {
 	
 	public static void main(String[] args) {
 		
-		Car car1 = new Car(2008, 15000, "green", 1200, "Toyota Prius");
-		Car car2 = new Car(2000, 9700, "white", 1600, "Renault Laguna"); 
+		Car toyotaPrius = new Car(2008, 15000, "green", 1200, "Toyota Prius");
+		Car renaultLaguna = new Car(2000, 9700, "white", 1600, "Renault Laguna");
+		
+		System.out.println(toyotaPrius.toString());
+		System.out.println(renaultLaguna.toString());
 		
 	}
 }
