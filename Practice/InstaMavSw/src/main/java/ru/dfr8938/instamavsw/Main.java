e00eae0251dc3fb9f35cc4c2a609ceafa5e55d34
Denis Frolov <d.fr8938@gmail.com>
2020-07-11 03:08:51 +0300

    change TextField on TextArea

--- Practice/InstaMavSw/src/main/java/ru/dfr8938/instamavsw/Main.java
+++ Practice/InstaMavSw/src/main/java/ru/dfr8938/instamavsw/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SetVisible setVisible = new SetVisible("InstaMavSw");
+        SetVisible setVisible = new SetVisible("InstaLoaderPhoto");
 
         setVisible.init();
     }
--- Practice/InstaMavSw/src/main/java/ru/dfr8938/instamavsw/SetVisible.java
+++ Practice/InstaMavSw/src/main/java/ru/dfr8938/instamavsw/SetVisible.java
@@ -26,6 +26,11 @@ public class SetVisible implements ActionListener {
     private JLabel usernameLabel;
     private JLabel loginLabel;
     private JLabel passLabel;
+    private JLabel infoLabel;
+    private JLabel userLabel;
+    private JLabel fullnameLabel;
+    private JLabel userOutputLabel;
+    private JLabel fullnameOutputLabel;
     private JLabel followersLabel;
     private JLabel followingLabel;
     private JLabel countFollowersLabel;
@@ -34,10 +39,11 @@ public class SetVisible implements ActionListener {
     private JTextField usernameTextField;
     private JTextField loginTextField;
     private JTextField passTextField;
-    private JTextField descriptionTextField;
+    private JTextArea descriptionTextField;
+    private JScrollPane scrollPane;
     private final JButton buttonEnter;
     private final JButton buttonClear;
-    private final JButton buttomAlbum;
+    private final JButton buttonAlbum;
 
     public SetVisible(String title) {
         this.title = title;
@@ -49,29 +55,35 @@ public class SetVisible implements ActionListener {
         this.usernameLabel = new JLabel("UserName");
         this.loginLabel = new JLabel("Login:");
         this.passLabel = new JLabel("Password:");
+        this.infoLabel = new JLabel("Info:");
+        this.userLabel = new JLabel("User Name:");
+        this.fullnameLabel = new JLabel("Full Name:");
+        this.userOutputLabel = new JLabel("-");
+        this.fullnameOutputLabel = new JLabel("-");
         this.followersLabel = new JLabel("Followers:");
         this.followingLabel = new JLabel("Following:");
         this.countFollowersLabel = new JLabel("0");
         this.countFollowingLabel = new JLabel("0");
         this.descriptionLabel = new JLabel("Description with album");
-        this.usernameTextField = new JTextField(10);
-        this.loginTextField = new JTextField(15);
-        this.passTextField = new JTextField(15);
-        this.descriptionTextField = new JTextField(15);
+        this.usernameTextField = new JTextField(25);
+        this.loginTextField = new JTextField(25);
+        this.passTextField = new JTextField(25);
+        this.descriptionTextField = new JTextArea(10, 2);
+        this.scrollPane = new JScrollPane(descriptionTextField);
         this.buttonEnter = new JButton("Enter");
         this.buttonClear = new JButton("Clear");
-        this.buttomAlbum = new JButton("Album");
+        this.buttonAlbum = new JButton("Album");
     }
 
     public void init() {
 
-        frame.setSize(300, 500);
+        frame.setSize(300, 550);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setLayout(new GridBagLayout());
 
         panelTop.add(usernameLabel, new GridBagConstraints(
-                0, 0, 1, 1, 1, 1,
+                0, 0, 2, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
@@ -103,43 +115,75 @@ public class SetVisible implements ActionListener {
         ));
         panelTop.setBackground(Color.LIGHT_GRAY);
 
-        panelCenter.add(followersLabel, new GridBagConstraints(
+        panelCenter.add(infoLabel, new GridBagConstraints(
                 0, 0, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
-        panelCenter.add(countFollowersLabel, new GridBagConstraints(
-                1, 0, 1, 1, 1, 1,
-                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
-                new Insets(2, 2, 2, 2), 0, 0
-        ));
-        panelCenter.add(followingLabel, new GridBagConstraints(
+        panelCenter.add(userLabel, new GridBagConstraints(
                 0, 1, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
-        panelCenter.add(countFollowingLabel, new GridBagConstraints(
+        panelCenter.add(userOutputLabel, new GridBagConstraints(
                 1, 1, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
+        panelCenter.add(fullnameLabel, new GridBagConstraints(
+                2, 1, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.add(fullnameOutputLabel, new GridBagConstraints(
+                3, 1, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.add(followersLabel, new GridBagConstraints(
+                0, 2, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.add(countFollowersLabel, new GridBagConstraints(
+                1, 2, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.add(followingLabel, new GridBagConstraints(
+                2, 2, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.add(countFollowingLabel, new GridBagConstraints(
+                3, 2, 1, 1, 1, 1,
+                GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
+                new Insets(2, 2, 2, 2), 0, 0
+        ));
+        panelCenter.setBackground(Color.LIGHT_GRAY);
+        infoLabel.setFont(new Font("Verdana", Font.ITALIC, 15));
+        infoLabel.setForeground(Color.WHITE);
 
         panelAlbum.add(descriptionLabel, new GridBagConstraints(
                 0, 0, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
-        panelAlbum.add(descriptionTextField, new GridBagConstraints(
+        descriptionTextField.setLineWrap(true);
+        descriptionTextField.setWrapStyleWord(true);
+        descriptionTextField.setAutoscrolls(true);
+        panelAlbum.add(scrollPane, new GridBagConstraints(
                 0, 1, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
-        panelAlbum.add(buttomAlbum, new GridBagConstraints(
+
+        panelAlbum.add(buttonAlbum, new GridBagConstraints(
                 0, 2, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0
         ));
-        buttomAlbum.addActionListener(this);
+        buttonAlbum.addActionListener(this);
         panelAlbum.setBackground(Color.LIGHT_GRAY);
 
         panelBottom.add(buttonEnter, new GridBagConstraints(
@@ -178,6 +222,7 @@ public class SetVisible implements ActionListener {
                 new Insets(2, 2, 2, 2), 0, 0
         ));
 
+        frame.setResizable(false);
         frame.setVisible(true);
         frame.pack();
     }
@@ -204,6 +249,11 @@ public class SetVisible implements ActionListener {
 
             countFollowersLabel.setText(String.valueOf(usernameResult.getUser().getFollower_count()));
             countFollowingLabel.setText(String.valueOf(usernameResult.getUser().getFollowing_count()));
+            userOutputLabel.setText(String.valueOf(usernameResult.getUser().getUsername()));
+            fullnameOutputLabel.setText(String.valueOf(usernameResult.getUser().getFull_name()));
+
+
+            System.out.println(usernameResult.getUser());
 
         } else if(((JButton) e.getSource()).getText().equals("Album")) {
             Instagram4j instagram = Instagram4j.builder().username(loginTextField.getText()).password(passTextField.getText()).build();
